07bfebf1027777d5713c779d8b3724ead67b62bb
devendraInfograins <devendralowanshi@infograins.com>
2026-01-07 14:30:24 +0530

    update prod and oher proxy

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,7 @@ const nextConfig = {
     parallelServerCompiles: true,
   },
   env: {
+    KONG_API_TARGET_URL: process.env.KONG_API_TARGET_URL,
     AZURE_TENANT_ID: process.env.AZURE_TENANT_ID,
     AZURE_CLIENT_ID: process.env.AZURE_CLIENT_ID,
     AZURE_CLIENT_SECRET: process.env.AZURE_CLIENT_SECRET,
